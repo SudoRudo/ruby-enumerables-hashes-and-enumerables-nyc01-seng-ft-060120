06c304af49ec1810775eae9f29de8f8d2bce763b
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-16 08:26:29 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,14 +7,5 @@
 # }
 
 def select_winner(passengers)
-  letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  i = 0
-  winner = ""
-  while i < letters.length do
-    passengers.each do |suite, name|
-      if suite == ":suite_#{letters[i]}" && name.start_with?("#{letters[i]}")
-        winner = name
-      end
-    i += 1
-  end
+ winner = ""
 end
